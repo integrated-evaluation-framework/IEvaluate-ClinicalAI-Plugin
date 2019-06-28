17739cb8246810ea84e589fbfc8eb781e28f2c52
Andrew Wen <Wen.Andrew@mayo.edu>
2019-06-28 17:16:05 -0500

    Register the clinical application provider at startup

--- src/main/java/edu/mayo/dhs/ievaluate/plugins/clinicalai/IEvaluateClinicalAIPlugin.java
+++ src/main/java/edu/mayo/dhs/ievaluate/plugins/clinicalai/IEvaluateClinicalAIPlugin.java
@@ -2,11 +2,13 @@ package edu.mayo.dhs.ievaluate.plugins.clinicalai;
 
 import edu.mayo.dhs.ievaluate.api.IEvaluate;
 import edu.mayo.dhs.ievaluate.api.plugins.IEvaluatePlugin;
+import edu.mayo.dhs.ievaluate.plugins.clinicalai.application.ClinicalApplicationProvider;
 import edu.mayo.dhs.ievaluate.plugins.clinicalai.assertions.ClinicalPhenotypingAssertionDefinition;
 
 public class IEvaluateClinicalAIPlugin extends IEvaluatePlugin {
     @Override
     public void onInit() {
+        IEvaluate.getServer().getApplicationManager().registerApplicationProvider(new ClinicalApplicationProvider());
         IEvaluate.getServer().registerAssertionDefinition(ClinicalPhenotypingAssertionDefinition.class);
     }
 }
